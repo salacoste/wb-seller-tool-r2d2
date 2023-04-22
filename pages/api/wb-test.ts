5c51f3d041d8c9f89a5127b9805911002491a946
Ivan D <bad_vano@list.ru>
2023-04-22 18:25:07 +0300

    basic WB api class method

---
+++ pages/api/wb-test.ts
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { WbModelReport } from '@/lib/wb-custom/index';
+
+type Data = {
+  data: any;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const srid = (req.query.srid as unknown as number) || 0;
+  const startDate = '2023-04-01';
+  const endDate = '2023-04-22';
+  const apiToken =
+    (req.query.api_token as unknown as string) ||
+    ('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjdlM2I5MjY5LWMyMGYtNDQ4OS1iZDYzLTYyMmUzZjkwMjRlYSJ9.pI_wUJqCg7sNYZh-EWV_sK7SLcqsLTvvGTM9-zFSbQ8' as unknown as string);
+  const realizationreport_id = req.query
+    .realizationreport_id as unknown as number;
+  console.log('ttt', realizationreport_id);
+  const response: any = await WbModelReport.getReport(
+    apiToken,
+    startDate,
+    endDate,
+    srid,
+    (req.query.limit as unknown as number) || 10000,
+    realizationreport_id,
+  );
+
+  Object.keys(response).forEach((key: any) => {
+    let item = response[key];
+    item.commision_percents =
+      (1 -
+        (item.total_sell_rub - item.total_delivery_rub) /
+          item.total_sell_before_commision) *
+      100;
+  });
+  res.status(200).json({ data: response });
+}
---
+++ src/lib/wb-custom/index.tsx
@@ -0,0 +1,118 @@
+import fs from 'fs';
+import xlsx, { read, writeFileXLSX } from 'xlsx';
+
+export interface IWbModel {
+  getReport: (
+    apiToken: string,
+    startDate: string,
+    endDate: string,
+    limit: number,
+    srid?: number,
+    realizationreport_id?: number | string,
+  ) => {};
+}
+
+export const WbModelReport: IWbModel = {
+  getReport: async (
+    apiToken: string,
+    startDate: string,
+    endDate: string,
+    srid = 0,
+    limit: number = 10000,
+    realizationreport_id?,
+    ...props
+  ) => {
+    srid = srid || 0;
+    const url = `https://statistics-api.wildberries.ru/api/v1/supplier/reportDetailByPeriod?dateFrom=${startDate}&limit=${limit}&dateTo=${endDate}&rrdid=${srid}`;
+
+    try {
+      let response = await fetch(url, {
+        headers: { Authorization: `Bearer ${apiToken}` },
+      });
+      var json: {}[] = await response.json();
+    } catch (error) {
+      console.log(error);
+      return {
+        error: (error as Error).message,
+      };
+    }
+    console.log('json', json);
+
+    if (json === null) {
+      return { error: 'Нет данных в ответе, проверьте заданные параметры' };
+    }
+    if (realizationreport_id) {
+      json = json.filter((x: { [k: string]: string }) => {
+        console.log(x.realizationreport_id);
+        return Number(x.realizationreport_id) === Number(realizationreport_id);
+      });
+      console.log(222, json);
+    }
+
+    // if (json.length === 0) {
+    //   return Object.fromEntries(new Map());
+    // }
+    let ws = xlsx.utils.json_to_sheet(json || {});
+    let wb = xlsx.utils.book_new();
+    xlsx.utils.book_append_sheet(wb, ws, 'Sales data');
+    xlsx.writeFile(wb, './data.xlsx');
+
+    // await fs.writeFile('./data.xls', xls, () => {});
+
+    const data = new Map();
+    // console.log(json);
+    json.forEach((item: { [k: string]: string | number }, i) => {
+      if (!(item.doc_type_name === 'Продажа')) {
+        console.log(`есть запись НЕ Продажа, а ${item.doc_type_name}`);
+        return false;
+      }
+
+      if (!data.has(item.sa_name)) {
+        const obj: { [k: string]: string | number } = {};
+        // console.log(item.sa_name);
+        if (item.supplier_oper_name === 'Продажа') {
+          obj.total_sell_quantity = item.quantity;
+          obj.total_sell_rub = item.ppvz_for_pay;
+          obj.total_sell_before_commision = item.retail_price_withdisc_rub;
+          obj.total_delivery_rub = 0;
+        }
+        if (
+          item.supplier_oper_name === 'Логистика' &&
+          item.bonus_type_name === 'К клиенту при продаже'
+        ) {
+          obj.total_delivery_rub = item.delivery_rub;
+        }
+        data.set(item.sa_name, {
+          ...obj,
+          id: item.barcode,
+          article: item.sa_name,
+          name: item.subject_name,
+        });
+      } else {
+        const obj = data.get(item.sa_name);
+        if (item.supplier_oper_name === 'Продажа') {
+          obj.total_sell_quantity += item.quantity;
+          obj.total_sell_rub += item.ppvz_for_pay;
+          obj.total_sell_before_commision += item.retail_price_withdisc_rub;
+        }
+        if (
+          item.supplier_oper_name === 'Логистика' &&
+          item.bonus_type_name === 'К клиенту при продаже'
+        ) {
+          obj.total_delivery_rub += Number(item.delivery_rub);
+          //   console.log(222, obj.total_delivery_rub);
+        }
+        data.set(item.sa_name, {
+          ...obj,
+          id: item.sa_name,
+          name: item.subject_name,
+          article: item.sa_name,
+        });
+      }
+    });
+
+    return await Object.fromEntries(data);
+  },
+};
+
+export default WbModelReport;
